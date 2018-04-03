15f02be1f3c5dc55c08f5b23e538673fab88209a
ultrox <crashxx@gmail.com>
2018-04-03 16:20:01 +0200

    Add reselect memoized libery to use with selectors
    What problem reselect solved:
    https://youtu.be/frT3to2ACCw?t=588
    
    Implementation:
    https://youtu.be/frT3to2ACCw?t=648

--- selectors/src/reducers/index.js
+++ selectors/src/reducers/index.js
@@ -1,19 +1,21 @@
-import { combineReducers } from 'redux';
-import { createSelector } from 'reselect';
-import auth, * as fromAuth from './auth'; // this was changed
-import rooms from './rooms';
+import { combineReducers } from "redux";
+import { createSelector } from "reselect";
+import auth, * as fromAuth from "./auth"; // this was changed
+import rooms from "./rooms";
 
 const reducer = combineReducers({
   auth,
-  rooms
+  rooms,
 });
 
-// simplifiing design 
-export const selectUserName = state => fromAuth.selectUserName(state.auth)
+// simplifiing design
+export const selectUserName = state => fromAuth.selectUserName(state.auth);
+export const selectUserReservation = state => state.auth.user.reservation;
+export const selectRoomList = state => state.rooms.list;
 
-export const selectUserRoom = state => {
-  const userReservation = state.auth.user.reservation;
-  const roomList = state.rooms.list;
-  return roomList.find(room => room.id === userReservation.roomType)
-}
+export const selectUserRoom = createSelector(
+  selectUserReservation,
+  selectRoomList,
+  (userReservation, roomList) => roomList.find(room => room.id === userReservation.roomType)
+);
 export default reducer;
