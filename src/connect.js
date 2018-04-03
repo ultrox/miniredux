4691e92786873650c7ba72e95cdc9d734d044562
ultrox <crashxx@gmail.com>
2018-04-03 11:34:37 +0200

    Map dispatch functions and state to end component

--- src/connect.js
+++ src/connect.js
@@ -1,10 +1,14 @@
-// connect is function that returnes
 function connect(mapStateToProps, mapDispatchToProps) {
+  // function which takes a React component as input and returns a new enhanced React component.
   return function(WrappedComponent) {
     // will return container component
     return class ANONcontainer extends React.Component {
       render() {
-        return <WrappedComponent {...this.props} />;
+        return <WrappedComponent 
+          {...this.props} 
+          {...mapStateToProps(store.getState(), this.props)}
+          {...mapDispatchToProps(store.dispatch, this.props)}
+          />
       }
     }
   };
