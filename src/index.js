76d7497136a6b2b42297528ea4cade4202807bed
ultrox <crashxx@gmail.com>
2018-04-03 10:58:11 +0200

    Add listener aka subscrib

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 function createStore(reducer, state ) {
   let currentState = state || {};
   const currentReducer = reducer;
-
+  const listener = () => {};
   return {
     getState: () => currentState,
     dispatch: action => {
       currentState = currentReducer(currentState, action)
+      listener(); // magic
       return action
-    }
+    },
+    subscribe: newListener => listener = newListener;
   }
 }
